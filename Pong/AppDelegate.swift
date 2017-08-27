46a10f98c882bd52dbfeac6d864ad8bd38de747d
Joe Holt <wynatoej@gmail.com>
2017-08-27 13:37:28 -0500

    Initial Commit

---
+++ Pong/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Pong
+//
+//  Created by Joe Holt on 8/27/17.
+//  Copyright © 2017 Joe Holt. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
