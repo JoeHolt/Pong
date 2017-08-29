8d6c7ab5749000a35d871a0844c7a14bf845f57b
Joe Holt <wynatoej@gmail.com>
2017-08-29 12:43:16 -0500

    Updated drawing code

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
         case Multi
     }
     
-    private let gridSize: Int       = 40    //Size of game grid
+    private let gridSize: Int       = 30    //Size of game grid
     private let updateGameBall: Int = 05    //Update game every x frames
     private let updateBump: Int     = 04    //Update speed for right bumper
     private var gameMode: GameMode  = .Multi //Game mode of game
@@ -40,7 +40,7 @@ class GameScene: SKScene {
         self.mainView = view
         self.ball = GameObject(CGSize(width: 1.0, height: 1.0), initialPosistion: CGPoint(x: -15, y: 0), initialSpeed: CGPoint(x: 1, y: 1))
         self.lBumper = GameObject(CGSize(width: 1.0, height: 4.0), initialPosistion: CGPoint(x: -gridBounds + 2, y: 0), initialSpeed: CGPoint(x: 0, y: -1))
-        self.rBumper = GameObject(CGSize(width: 1.0, height: 4.0), initialPosistion: CGPoint(x: gridBounds - 2, y: 0), initialSpeed: CGPoint(x: 0, y: -1))
+        self.rBumper = GameObject(CGSize(width: 1.0, height: 4.0), initialPosistion: CGPoint(x: gridBounds - 3, y: 0), initialSpeed: CGPoint(x: 0, y: -1))
         self.scoreLabel = SKLabelNode(text: "0")
         self.scoreLabel.fontSize = 500
         self.scoreLabel.alpha = 0.5
@@ -142,7 +142,7 @@ class GameScene: SKScene {
     private func updateBumper(_ bumper: GameObject) {
         let gridBound = self.gridBounds - (self.gridBounds * 0.1)   //Makes the grid line up with the window better
         //Left user bumper
-        if bumper.speed.y != 0 && bumper.posistion.y < gridBound && bumper.posistion.y > -gridBound {
+        if bumper.speed.y != 0 && bumper.posistion.y < gridBound-2 && bumper.posistion.y > -(gridBound-1) {
             let newPos = CGPoint(x: bumper.posistion.x, y: bumper.posistion.y + bumper.speed.y)
             bumper.posistion = newPos
             bumper.body.insert(newPos, at: 0)
@@ -168,10 +168,10 @@ class GameScene: SKScene {
             score = 0
             ball.speed = CGPoint(x: 1, y: 1)
         }
-        if newBallPosistion.y > gridBound {
+        if newBallPosistion.y >= gridBound-1 {
             ball.speed.y = -1
         }
-        if newBallPosistion.y < -gridBound {
+        if newBallPosistion.y <= -gridBound {
             ball.speed.y = 1
         }
         //Check if ball is about to hit bumper and act accorgingly
@@ -186,19 +186,18 @@ class GameScene: SKScene {
     }
     
     private func drawSquare(x: Int, y: Int, color: SKColor = SKColor.white) {
-        let height = mainView.frame.size.height/CGFloat(gridSize)
-        let xDim = (mainView.frame.size.width/CGFloat(gridSize)) * CGFloat(x)
-        let yDim = (mainView.frame.size.height/CGFloat(gridSize)) * CGFloat(y)
+        let xDim = (x * gridSize) + gridSize/2
+        let yDim = (y * gridSize) + gridSize/2
         let square = SKSpriteNode()
         square.position = CGPoint(x: xDim, y: yDim)
-        square.size = CGSize(width: height - 2, height: height - 2)
+        square.size = CGSize(width: gridSize-2, height: gridSize-2)
         square.color = color
         addChild(square)
     }
     
     private func removeSquare(x: Int, y: Int) {
-        let xDim = (mainView.frame.size.width/CGFloat(gridSize)) * CGFloat(x)
-        let yDim = (mainView.frame.size.height/CGFloat(gridSize)) * CGFloat(y)
+        let xDim = (x * gridSize) + gridSize/2
+        let yDim = (y * gridSize) + gridSize/2
         let nodesAtPoint = nodes(at: CGPoint(x: xDim, y: yDim))
         for node in nodesAtPoint {
             if node is SKLabelNode {} else {
